03d7ad0017c67193c97c4e4d32872336ce949e9e
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-11-18 07:03:51 -0600

    cleanup

--- graytocolor/UserFuncEval.py
+++ graytocolor/UserFuncEval.py
@@ -1,12 +1,19 @@
 import numpy as np
 
 import simpleeval   # https://github.com/danthedeckie/simpleeval
-# pip install simpleeval
+"""
+$ pip install simpleeval
+"""
 
 class UserFuncEval:
     def __init__(self):
         other_functions = {"sin": np.sin, "cos": np.cos, "tan": np.tan, "abs": abs}
+        other_functions.update({"mod": np.mod, "sign": np.sign})
         self.s = simpleeval.SimpleEval()
+        self.s.functions = simpleeval.DEFAULT_FUNCTIONS.copy()
+        del self.s.functions["str"]
+        del self.s.functions["rand"]
+        del self.s.functions["randint"]
         self.s.functions.update(other_functions)
         self.s.names = {"x": np.arange(256), "pi": np.pi}
         self.output = None
@@ -14,11 +21,9 @@ class UserFuncEval:
     # input is a string of the user input function
     # returns false if unsuccessful parse. maybe set input font color to red while there is invalid input?
     def update(self, input, var_substitutions = None):
-        if var_substitutions:  # and type(var_substitutions) == type(dict()):
-            self.s.names = var_substitutions
-            # print("var_substitutions")
+        if var_substitutions:
+            self.s.names.update(var_substitutions)
         
-        # self.output = self.s.eval(input)
         try:
             self.output = self.s.eval(input)
         except:
@@ -28,3 +33,6 @@ class UserFuncEval:
 
     def getOutput(self):
         return self.output
+
+    def getValidOperations(self):
+        return set(self.s.functions.keys())
